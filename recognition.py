e783e5b0f38d714aea3074c61975297d8f27ccdf
Shuming Fang <fangshuming519@gmail.com>
2017-12-17 21:33:50 +0800

    Add files via upload

---
+++ recognition.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+
+# obtain path to "english.wav" in the same folder as this script
+from os import path
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "out.wav")
+# AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "french.aiff")
+#AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "chinese.flac")
+#AUDIO_FILE = 'E:\\jackma\\speech_recognition-master\\speech_recognition-master\\examples\\hi.wav'
+# use the audio file as the audio source
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # read the entire audio file
+result = r.recognize_google(audio)
+print(result)

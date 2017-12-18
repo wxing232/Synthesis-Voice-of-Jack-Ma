f6aa629298331f9e146dfad812572bf63df5e227
Shuming Fang <fangshuming519@gmail.com>
2017-12-18 19:36:37 +0800

    Create bing.py

---
+++ bing.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import speech_recognition as sr
+
+# obtain path to "english.wav" in the same folder as this script
+from os import path
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "english.wav")
+
+# use the audio file as the audio source
+r = sr.Recognizer()
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source)  # read the entire audio file
+# recognize speech using Microsoft Bing Voice Recognition
+BING_KEY = "4a523f15a13341a2a0ccbe0ae278defd"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+try:
+    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
+except sr.UnknownValueError:
+    print("Microsoft Bing Voice Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))

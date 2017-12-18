a1cb4a545d1856797cc4098aa458407142629a8c
Shuming Fang <fangshuming519@gmail.com>
2017-12-18 21:21:06 +0800

    Update bing.py

--- bing.py
+++ bing.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
-
+# -*- coding: utf-8 -*-
 import speech_recognition as sr
-
-# obtain path to "english.wav" in the same folder as this script
+import time
+import os
+import numpy as np
 from os import path
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "english.wav")
 
-# use the audio file as the audio source
+def save_to_file(file_name, contents):
+    fh = open(file_name, 'w')
+    fh.write(contents)
+    fh.close()
+
+path = 'E:\\jackma\\sliced\\'
+files = os.listdir(path)
 r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
-    audio = r.record(source)  # read the entire audio file
-# recognize speech using Microsoft Bing Voice Recognition
-BING_KEY = "BING_KEY"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+for file in files[:3]:
+# use the audio file as the audio source
+    #time.sleep(3)
+    with sr.AudioFile(path+file) as source:
+        audio = r.record(source)  # read the entire audio file
+    # recognize speech using Microsoft Bing Voice Recognition
+    BING_KEY = "BING_KEY"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+    try:
+        result = r.recognize_bing(audio, key=BING_KEY)
+        save_to_file(file[:10]+'.txt', result)
+    except:
+        print('failed')
